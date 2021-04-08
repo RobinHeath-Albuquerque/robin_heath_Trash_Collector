11d9606c37097d1666a4877b0eacbcef659fb69a
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-04-08 12:29:19 -0600

    Created Customer and Employee Models and Apps

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+
+
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default=0)
     user = models.ForeignKey('accounts.User', default=0, on_delete=models.CASCADE)
+    zip_code = models.IntegerField(default=0)
+    email = models.CharField(max_length=50, default=0)
+
+    def __str__(self):
+        return self.name
--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employees(models.Model):
+    name = models.CharField(max_length=50, default=0)
+    zip_code = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
