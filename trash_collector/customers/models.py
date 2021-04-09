de656890bb994d7d936f7be72ce1020a79e2f45d
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-04-09 13:52:24 -0600

    Changed customers/models.py

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -9,8 +9,9 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50, default=0)
     user = models.ForeignKey('accounts.User', default=0, on_delete=models.CASCADE)
-    zip_code = models.IntegerField(default=0)
-    email = models.CharField(max_length=50, default=0)
+    address = models.CharField(max_length=50, default=0)
+    zip_code = models.CharField(max_length=5, default=0)
+    service_day = models.CharField(max_length=50, default=0)
 
     def __str__(self):
         return self.name
