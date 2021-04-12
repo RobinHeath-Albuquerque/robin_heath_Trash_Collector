3eba321e8e0c9f7e84de869b74debfcf353f84ca
richardpphilip <richardpphilip1@gmail.com>
2021-04-12 09:22:36 -0700

    enabled the employee render to display people with matching zip codes.  tested and the code works.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -2,25 +2,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
 from django.urls import reverse
-
 from .models import Employees
-
-
 # Create your views here.
-
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
-
-
 def index(request):
     # Get the Customer model from the other app, it can now be used to query the db
     employees = apps.get_model('employees.Employees')
-
     return render(request, 'employees/index.html')
-
-
 def localzip_employee(request):
     user = request.user
-    employee = Employees.object.get(user_id=user.id)
+    employee = Employees.objects.get(user_id=user.id)
     Customer: object = apps.get_model('customers.Customer')
     customers = Customer.objects.all()
     same_zipcode = []
@@ -31,18 +22,12 @@ def localzip_employee(request):
                 'customers': same_zipcode
             }
     return render(request, 'employees/index.html', context)
-
-
 def one_time_pick_up_due_out(request):
     context = {}
     return render(request, 'employees/one_time_pick_up_due_out.html', context)
-
-
 def active_accounts(request):
     context = {}
     return render(request, 'employees/active_accounts.html', context)
-
-
 def create(request):
     if request.method == 'POST':
         user = request.user
