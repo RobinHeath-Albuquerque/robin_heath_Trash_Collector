4966faa5dae7cc528b7ea8b57f784d59139794ff
richardpphilip <richardpphilip1@gmail.com>
2021-04-13 09:48:21 -0700

    enabled the employee to search for people that have pickups on the specified day of the week.  Also created a function that displays one time pick ups that match todays date.

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -4,15 +4,9 @@ from . import views
 app_name = "employees"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('localzip_employee_monday/', views.localzip_employee_monday, name='localzip_employee_monday'),
-    path('localzip_employee_tuesday/', views.localzip_employee_tuesday, name='localzip_employee_tuesday'),
-    path('localzip_employee_wednesday/', views.localzip_employee_wednesday, name='localzip_employee_wednesday'),
-    path('localzip_employee_thursday/', views.localzip_employee_thursday, name='localzip_employee_thursday'),
-    path('localzip_employee_friday/', views.localzip_employee_friday, name='localzip_employee_friday'),
-    path('localzip_employee_saturday/', views.localzip_employee_saturday, name='localzip_employee_saturday'),
-    path('localzip_employee_sunday/', views.localzip_employee_sunday, name='localzip_employee_sunday'),
-    path('same_service_day_employee/', views.same_service_day_employee, name='same_service_day_employee'),
-    path('one_time_pick_up_due_out/', views.one_time_pick_up_due_out, name='one_time_pick_up_due_out'),
+    path('define_day/', views.define_day, name='define_day'),
+    path('localzip_employee/', views.localzip_employee, name='localzip_employee'),
+    path('one_time_pick_up/', views.one_time_pick_up, name='one_time_pick_up'),
     path('active_accounts/', views.active_accounts, name='active_accounts'),
     path('create/', views.create, name='create')
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.apps import apps
 from django.urls import reverse
 from .models import Employees
+from datetime import date
+import datetime
 
 
 # Create your views here.
@@ -13,97 +15,7 @@ def index(request):
     return render(request, 'employees/index.html')
 
 
-def localzip_employee_monday(request):
-    user = request.user
-    employee = Employees.objects.get(user_id=user.id)
-    Customer: object = apps.get_model('customers.Customer')
-    customers = Customer.objects.all()
-    service_day_monday = 'monday'
-    same_zipcode = []
-    for customer in customers:
-        if customer.zip_code == employee.zip_code and customer.service_day == service_day_monday:
-            same_zipcode.append(customer)
-            context = {
-                'customers': same_zipcode
-            }
-        else:
-            context = {}
-    return render(request, 'employees/index.html', context)
-
-
-def localzip_employee_tuesday(request):
-    user = request.user
-    employee = Employees.objects.get(user_id=user.id)
-    Customer: object = apps.get_model('customers.Customer')
-    customers = Customer.objects.all()
-    service_day_tuesday = 'tuesday'
-    same_zipcode = []
-    for customer in customers:
-        if customer.zip_code == employee.zip_code and customer.service_day == service_day_tuesday:
-            same_zipcode.append(customer)
-            context = {
-                'customers': same_zipcode
-            }
-        else:
-            context = {}
-    return render(request, 'employees/index.html', context)
-
-
-def localzip_employee_wednesday(request):
-    user = request.user
-    employee = Employees.objects.get(user_id=user.id)
-    Customer: object = apps.get_model('customers.Customer')
-    customers = Customer.objects.all()
-    service_day_wednesday = 'wednesday'
-    same_zipcode = []
-    for customer in customers:
-        if customer.zip_code == employee.zip_code and customer.service_day == service_day_wednesday:
-            same_zipcode.append(customer)
-            context = {
-                'customers': same_zipcode
-            }
-        else:
-            context = {}
-    return render(request, 'employees/index.html', context)
-
-
-def localzip_employee_thursday(request):
-    user = request.user
-    employee = Employees.objects.get(user_id=user.id)
-    Customer: object = apps.get_model('customers.Customer')
-    customers = Customer.objects.all()
-    service_day_thursday = 'thursday'
-    same_zipcode = []
-    for customer in customers:
-        if customer.zip_code == employee.zip_code and customer.service_day == service_day_thursday:
-            same_zipcode.append(customer)
-            context = {
-                'customers': same_zipcode
-            }
-        else:
-            context = {}
-    return render(request, 'employees/index.html', context)
-
-
-def localzip_employee_friday(request):
-    user = request.user
-    employee = Employees.objects.get(user_id=user.id)
-    Customer: object = apps.get_model('customers.Customer')
-    customers = Customer.objects.all()
-    service_day_friday = 'friday'
-    same_zipcode = []
-    for customer in customers:
-        if customer.zip_code == employee.zip_code and customer.service_day == service_day_friday:
-            same_zipcode.append(customer)
-            context = {
-                'customers': same_zipcode
-            }
-        else:
-            context = {}
-    return render(request, 'employees/index.html', context)
-
-
-def localzip_employee_saturday(request):
+def localzip_employee(request):
     user = request.user
     employee = Employees.objects.get(user_id=user.id)
     Customer: object = apps.get_model('customers.Customer')
@@ -111,43 +23,22 @@ def localzip_employee_saturday(request):
     service_day_saturday = 'saturday'
     same_zipcode = []
     for customer in customers:
-        if customer.zip_code == employee.zip_code and customer.service_day == service_day_saturday:
+        if customer.zip_code == employee.zip_code and customer.service_day == employee.define_day:
             same_zipcode.append(customer)
             context = {
                 'customers': same_zipcode
             }
-        else:
-            context = {}
     return render(request, 'employees/index.html', context)
 
 
-def localzip_employee_sunday(request):
+def one_time_pick_up(request):
     user = request.user
     employee = Employees.objects.get(user_id=user.id)
-    Customer: object = apps.get_model('customers.Customer')
-    customers = Customer.objects.all()
-    service_day_sunday = 'sunday'
-    same_zipcode = []
-    for customer in customers:
-        if customer.zip_code == employee.zip_code and customer.service_day == service_day_sunday:
-            same_zipcode.append(customer)
-            context = {
-                'customers': same_zipcode
-            }
-        else:
-            context = {}
-    return render(request, 'employees/index.html', context)
-
-
-def same_service_day_employee(request):
-    user = request.user
-    day_of_the_week = 'saturday'
-    employee = Employees.objects.get(user_id=user.id)
-    Customer: object = apps.get_model('customers.Customer')
+    Customer = apps.get_model('customers.Customer')
     customers = Customer.objects.all()
     same_service_day = []
     for customer in customers:
-        if customer.service_day == day_of_the_week:
+        if customer.one_time_day == date.today() and customer.zip_code == employee.zip_code:
             same_service_day.append(customer)
             context = {
                 'customers': same_service_day
@@ -155,9 +46,15 @@ def same_service_day_employee(request):
     return render(request, 'employees/index.html', context)
 
 
-def one_time_pick_up_due_out(request):
-    context = {}
-    return render(request, 'employees/one_time_pick_up_due_out.html', context)
+def define_day(request):
+    if request.method == 'POST':
+        user = request.user
+        employee = Employees.objects.get(user_id=user.id)
+        employee.define_day = request.POST.get('define_day')
+        employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+
+    return render(request, 'employees/index.html')
 
 
 def active_accounts(request):
@@ -178,3 +75,6 @@ def create(request):
         return render(request, 'employees/create.html')
 
 
+def showdate(request):
+    datetime.datetime.now()
+    return render(request, 'employees/index.html')
